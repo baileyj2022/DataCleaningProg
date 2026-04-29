04bbae09f297581ad0013e5fdbe85cc320f844da
jemimagayy <jgay2022@fau.edu>
2026-04-29 01:09:13 -0400

    Fix db.py to use explicit database name

--- backend/db.py
+++ backend/db.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
-db = client.get_default_database()  # will use "business_data" from the URI
+db = client[os.getenv("DB_NAME", "cleaning_jobs")]
